063726a0fd96654acdf0b7ab1d9ba0149259885b
Russ Albright <russell.albright@gmail.com>
2023-03-31 17:09:03 -0400

    Add files via upload

--- site.py
+++ site.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 
 class Site:
-    def __init__(self , source , dest):
+    def __init__(self , source , dest,parsers=None):
+        self.parsers= parsers
         self.source=Path(source)
         self.dest=Path(dest)
 
@@ -15,8 +16,8 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-            # elif path.is_file():
-            #     self.run_parser(path)
+            elif path.is_file():
+                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
@@ -28,7 +29,8 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
+            NotImplemented
+            #self.error("No parser for the {} extension, file skipped!".format(path.suffix))
 
     @staticmethod
     def error(message):
